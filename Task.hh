9886aa63d64d26686f66977ae980d335a31c3143
ShaguftaKhizar <shagufta.khizar@outlook.fr>
2017-01-10 07:07:19 +0100

    compile pas, pas finit

--- Task.hh
+++ Task.hh
@@ -4,6 +4,8 @@
 #include "Jury_Team.hh"
 #include "Killer.hh"
 #include "ProMember.hh"
+#include "Jury.hh"
+#include "ProMember.hh"
 
 #include <iostream>
 #include <typeinfo>
@@ -30,26 +32,36 @@ template <class T,class U>
 int Task::surprise_challenge(Killer<T>& k1, Victim<U>& v1, std::string abilityTested1, std::string abilityTested2){
 
 	vector<Candidat> lc1 = v1.getTeamMember();
+	vector<Jury> lj;
+	vector<ProMember> lp;
+	int index = 0;
+
+	if(typeid(k1) == typeid(Jury_Team<T>))
+		lj = k1.getTeamMember();
+	if(typeid(k1) == typeid(ProductionHouseTeam<T>))
+		lp = k1.getTeamMember();
 
 	for(auto it1 : lc1)
 	{
+		index = rand()%(lp.size());
+
 		if((it1.getAbilities().find(abilityTested1)->second) >= 50)
 		{
-			if((k1.getProAbilities().find(abilityTested2)->second) >= 50)
+			if((lp[index].getProAbilities().find(abilityTested2)->second) >= 50)
 				return 0;
 
 		}
 
 		if((it1.getAbilities().find(abilityTested1)->second) <= 50)
 		{
-			if((k1.getProAbilities().find(abilityTested2)->second) >= 50)
+			if((lp[index].getProAbilities().find(abilityTested2)->second) >= 50)
 				return k1.getTeamNumber();
 
 		}
 
 		if((it1.getAbilities().find(abilityTested1)->second) >= 50)
 		{
-			if((k1.getProAbilities().find(abilityTested2)->second) <= 50)
+			if((lp[index].getProAbilities().find(abilityTested2)->second) <= 50)
 				return v1.getTeamNumber();
 
 		}
@@ -64,8 +76,7 @@ template <class T>
 int Task::fashion_show_challenge(Victim<T>& v1, Victim<T>& v2, string abilityTested){
 
 	vector<Candidat> lc1 = v1.getTeamMember();
-	vector<Candidat> lc2 = v2.getTeamMember();
-
+		
 	for(auto it1 : lc1)
 	{
 		for(auto it2 : lc2)
